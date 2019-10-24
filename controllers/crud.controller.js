f1552da7d29598aee6ed312fcec97542b6e5ca72
Jesse Kiilamaa <l4657@student.jamk.fi>
2019-10-24 16:07:53 +0300

    Function to create a PageContent document and add it to a Page document

--- controllers/crud.controller.js
+++ controllers/crud.controller.js
@@ -1,46 +1,105 @@
+/**
+ * Controller for the CRUD operations of the application.
+ */
+
 const { PageModel, PageContentModel, SupportSourceModel } 
     = require('../models/Models');
 
+
+// Get and return all the Page documents.
 exports.getAllPages = (req, res) => {
     PageModel.find()
         .then(doc => {
             return res.status(200).send(doc);
         }).catch(err => {
-            res.status(500).send({
+            return res.status(500).send({
                 message: err.message
             });
         });
 };
 
+/**
+ * Create a new Page document.
+ * The request body should be JSON and have a "pageName" field.
+ */
 exports.createPage = async (req, res) => {
     if (req.body.pageName === undefined) {
         return res.status(400).send({
-            message: 'Request body needs field \'pageName\''
+            message: "Request body needs field 'pageName'."
         });
     }
 
+    // Set the order number depending on the number of existing documents.
     let count;
     await PageModel.estimatedDocumentCount((err, docCount) => {
         if (err) {
-            res.status(500).send({
+            return res.status(500).send({
                 message: err.message
             });
         }
-        count = docCount;
+        count = docCount + 1;
     });
 
+    // Create a new Page document from the model.
     let page = new PageModel({
         pageName: req.body.pageName,
         pageContent: [],
         orderNumber: count !== undefined ? count : 1
     });
 
+    // Save the new document.
     page.save()
         .then(doc => {
             return res.status(200).send(doc);
         }).catch(err => {
-            res.status(500).send({
-                message: err.message || 'Some error occurred when saving data'
+            return res.status(500).send({
+                message: err.message || "Some error occurred when saving data"
             });
         });
+}
+
+/**
+ * Create a new PageContent document inside a Page document.
+ * The request body should be JSON and have a "name" field.
+ * The request parameter needs to be the ObjectID of the parent
+ * document.
+ */
+exports.createPageContent = async (req, res) => {
+    if (
+        req.body.name === undefined ||
+        req.params.id === undefined
+        ) {
+        return res.status(400).send({
+            message: "Request body needs field 'name'."
+        });
+    }
+
+    let pageDoc;
+    await PageModel.findById(req.params.id)
+        .then(doc => {
+            pageDoc = doc;
+        }).catch(err => {
+            return res.status(500).send({
+                message: err.message
+            });
+        });
+
+    let count = pageDoc.pageContent.length + 1;
+
+    let pageContent = new PageContentModel({
+        name: req.body.name,
+        supportSources: [],
+        orderNumber: count !== undefined ? count : 1
+    });
+
+    pageDoc.pageContent.push(pageContent);
+    pageDoc.save()
+        .then(doc => {
+            return res.status(200).send(doc);
+        }).catch(err => {
+            return res.status(500).send({
+                message: err.message || "Some error occurred when saving data"
+            });
+        });
+
 }
--- routes/crud.routes.js
+++ routes/crud.routes.js
@@ -6,10 +6,13 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../controllers/crud.controller');
 
-// Get all the pages
+// Get all the Pages
 router.get('/pages', Controller.getAllPages);
 
-// Create a new page
+// Create a new Page
 router.post('/pages', Controller.createPage);
+
+// Create a new PageContent inside a Page
+router.post('/pages/:id/page-contents', Controller.createPageContent);
 
 module.exports = router;
